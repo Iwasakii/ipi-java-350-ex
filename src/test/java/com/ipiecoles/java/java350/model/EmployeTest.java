15cad8be4c64d1082a2284f0387f7117ca45fa69
Tomy-sama <tom_nguyen0@hotmail.fr>
2020-04-16 14:49:55 +0200

    folder

---
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -0,0 +1,146 @@
+package com.ipiecoles.java.java350.model;
+
+//import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.time.LocalDate;
+
+public class EmployeTest extends Assertions {
+
+    @Test
+    public void testNbAnneeAncienneteNow() {
+
+        // Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now());
+        // When
+        Integer nbAnnees = employe.getNombreAnneeAnciennete();
+        // Then
+//        assertEquals(0, nbAnnees);
+        Assertions.assertThat(0).isEqualTo(nbAnnees);
+
+    }
+
+    @Test
+    public void testNbAnneeAncienneteNowMoins2() {
+
+        // Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().minusYears(2));
+        // When
+        Integer nbAnnees = employe.getNombreAnneeAnciennete();
+        // Then
+        Assertions.assertThat(2).isEqualTo(nbAnnees);
+    }
+
+    @Test
+    public void testNbAnneeAncienneteNowPlus3() {
+
+        // Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().plusYears(3));
+        // When
+        Integer nbAnnees = employe.getNombreAnneeAnciennete();
+        // Then
+//        assertEquals(0,nbAnnees);
+        Assertions.assertThat(0).isEqualTo(nbAnnees);
+    }
+
+    @Test
+    public void getNombreAnneeAncienneteNplus2(){
+        //Given
+        Employe e = new Employe();
+        e.setDateEmbauche(LocalDate.now().plusYears(2L));
+
+        //When
+        Integer anneeAnciennete = e.getNombreAnneeAnciennete();
+
+        //Then
+//        assertEquals(-2, anneeAnciennete.intValue());
+        Assertions.assertThat(0).isEqualTo(anneeAnciennete.intValue());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 'T12345', 0, 1.0, 1000.0",
+            "1, 'T12345', 2, 0.5, 600.0",
+            "1, 'T12345', 2, 1.0, 1200.0",
+            "2, 'T12345', 0, 1.0, 2300.0",
+            "2, 'T12345', 1, 1.0, 2400.0",
+            "1, 'M12345', 0, 1.0, 1700.0",
+            "1, 'M12345', 5, 1.0, 2200.0",
+            "2, 'M12345', 0, 1.0, 1700.0",
+            "2, 'M12345', 8, 1.0, 2500.0"
+    })
+    public void getPrimeAnnuelle(Integer performance, String matricule, Long nbYearsAnciennete, Double tempsPartiel, Double primeAnnuelle){
+        //Given
+        Employe employe = new Employe("Doe", "John", matricule, LocalDate.now().minusYears(nbYearsAnciennete), Entreprise.SALAIRE_BASE, performance, tempsPartiel);
+
+        //When
+        Double prime = employe.getPrimeAnnuelle();
+
+        //Then
+//        Assertions.assertEquals(primeAnnuelle, prime);
+        Assertions.assertThat(primeAnnuelle).isEqualTo(prime);
+
+    }
+
+    @Test
+    public void testaugmenterSalaire() throws Exception {
+        // Given
+        Employe employe = new Employe("Doe", "John", "C0000", LocalDate.now(), 1000.0, 1, 10.0);
+
+        // When
+        employe.augmenterSalaire(20);
+
+        // Then
+        Assertions.assertThat(employe.getSalaire()).isEqualTo(1200);
+    }
+
+    @Test
+    public void testaugmenterSalaireNegatif() {
+        // Given
+        Employe employe = new Employe("Doe", "John", "C0000", LocalDate.now(), 1000.0, 1, 10.0);
+
+        // When // Then
+        Assertions.assertThatThrownBy(() -> {
+            employe.augmenterSalaire(-5);
+                }
+        )
+                .isInstanceOf(Exception.class)
+                .hasMessage("Le pourcentage ne peut pas être négatif");
+
+    }
+
+    @Test
+    public void testaugmenterSalaireNull() {
+        // Given
+        Employe employe = new Employe("Doe", "John", "C0000", LocalDate.now(), null, 1, 10.0);
+
+        // When // Then
+        Assertions.assertThatThrownBy(() -> {
+                    employe.augmenterSalaire(2);
+                }
+        )
+                .isInstanceOf(Exception.class)
+                .hasMessage("Le salaire est nulle");
+    }
+
+    @Test
+    public void testaugmenterSalairePourcentage0() {
+        // Given
+        Employe employe = new Employe("Doe", "John", "C0000", LocalDate.now(), 1000.0, 1, 10.0);
+
+        // When // Then
+        Assertions.assertThatThrownBy(() -> {
+                    employe.augmenterSalaire(0);
+                }
+        )
+                .isInstanceOf(Exception.class)
+                .hasMessage("Le pourcentage ne peut pas être 0");
+    }
+
+}
---
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeServiceIntegrationTest.java
@@ -0,0 +1,98 @@
+package com.ipiecoles.java.java350.repository;
+
+import com.ipiecoles.java.java350.exception.EmployeException;
+import com.ipiecoles.java.java350.model.Employe;
+import com.ipiecoles.java.java350.model.Entreprise;
+import com.ipiecoles.java.java350.model.NiveauEtude;
+import com.ipiecoles.java.java350.model.Poste;
+import com.ipiecoles.java.java350.service.EmployeService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@SpringBootTest
+public class EmployeServiceIntegrationTest {
+
+    @Autowired
+    EmployeRepository employeRepository;
+    @Autowired
+    EmployeService employeService;
+
+    @BeforeEach
+    void setup(){
+        employeRepository.deleteAll();
+    }
+
+    @Test
+    public void integrationEmbaucheEmploye() throws EmployeException {
+        //Given
+        employeRepository.save(new Employe("Doe", "John", "T12345", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0));
+        String nom = "Doe";
+        String prenom = "John";
+        Poste poste = Poste.TECHNICIEN;
+        NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
+        Double tempsPartiel = 1.0;
+
+        //When
+        employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel);
+
+        //Then
+        Employe employe = employeRepository.findByMatricule("T12346");
+        Assertions.assertNotNull(employe);
+        Assertions.assertEquals(nom, employe.getNom());
+        Assertions.assertEquals(prenom, employe.getPrenom());
+        Assertions.assertEquals(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")), employe.getDateEmbauche().format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+        Assertions.assertEquals("T12346", employe.getMatricule());
+        Assertions.assertEquals(1.0, employe.getTempsPartiel().doubleValue());
+
+        //1521.22 * 1.2 * 1.0
+        Assertions.assertEquals(1825.46, employe.getSalaire().doubleValue());
+    }
+
+    @Test
+    public void integrationEmbaucheEmployePartiel() throws EmployeException {
+        //Given
+        employeRepository.save(new Employe("Doe", "John", "T12345", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0));
+        String nom = "Doe";
+        String prenom = "John";
+        Poste poste = Poste.TECHNICIEN;
+        NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
+        Double tempsPartiel = 0.5;
+
+        //When
+        employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel);
+
+        //Then
+        Employe employe = employeRepository.findByMatricule("T12346");
+        Assertions.assertNotNull(employe);
+        Assertions.assertEquals(nom, employe.getNom());
+        Assertions.assertEquals(prenom, employe.getPrenom());
+        Assertions.assertEquals(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")), employe.getDateEmbauche().format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+        Assertions.assertEquals("T12346", employe.getMatricule());
+        Assertions.assertEquals(0.5, employe.getTempsPartiel().doubleValue());
+
+        //1521.22 * 1.2 * 0.5
+        Assertions.assertEquals(912.73, employe.getSalaire().doubleValue());
+    }
+
+    @Test
+    void calculPerformanceCommercial() throws EmployeException {
+        // Given
+        String matricule = "C0001";
+        Employe employe = new Employe();
+        employe.setMatricule(matricule);
+        employe.setPerformance(5);
+        employeRepository.save(employe);
+        //When
+        employeService.calculPerformanceCommercial(matricule, 100L, 100L);
+        //Then
+        Employe employee = employeRepository.findByMatricule(matricule);
+        Assertions.assertEquals(5, employee.getPerformance());
+    }
+
+}
